07f652197437addbb97237794ab2b7fbed5d0e0f
Thiago F. Martins <thiagofm33@gmail.com>
2025-04-28 11:09:47 -0300

    Updated `__tests__/Statement/StatementSection.jsx`

--- __tests__/Statement/StatementSection.jsx
+++ __tests__/Statement/StatementSection.jsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import StatementSection from '../../@core/components/Statement/StatementSection';
 
-describe('Page', () => {
+describe('Statement Section', () => {
   it('renders a level 6 heading', () => {
     render(
         <StatementSection
