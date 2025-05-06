e05bbac06f9d14debb452de9ef90c99d21a39e1d
Thiago F. Martins <thiagofm33@gmail.com>
2025-05-06 15:00:37 -0300

    Mais testes unitários de componentes (#2)
    * Added tests for the balance component
    
    * Updated `__tests__/Statement/StatementItem.jsx`
    
    * Updated `__tests__/Statement/StatementSection.jsx`
    
    * Updated `__tests__/Statement/Statement.jsx`
    
    * Added tests for the loading component.
    
    * Updated `@core/components/Toast/ToastIconWrapper.tsx`
    
    * Added testes for the Toast component
    
    * Moved the components tests to the components folder
    
    * Updated `@core/components/Toast/index.tsx`
    
    * Updated `@core/components/ui/menu/Menu.tsx`

--- __tests__/components/Statement/Statement.jsx
+++ __tests__/components/Statement/Statement.jsx
@@ -1,9 +1,9 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Statement from '../../@core/components/Statement';
+import Statement from '../../../@core/components/Statement';
 
-describe('Page', () => {
+describe('Statement', () => {
   it('renders a level 6 heading', () => {
     render(
       <Statement
